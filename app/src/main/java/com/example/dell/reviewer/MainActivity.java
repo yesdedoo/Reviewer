28159c98af1be6693f2b76eea576b190ca4a6a64
Suvijak <yesdedoo@gmail.com>
2018-04-12 19:08:28 +0700

    Created DB but still not config.

--- app/src/main/java/com/example/dell/reviewer/MainActivity.java
+++ app/src/main/java/com/example/dell/reviewer/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.dell.reviewer;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
+
+    public void GotoLogin()
+    {
+        Intent intent = new Intent(this,LoginActivity.class);
+        startActivity(intent);
+    }
+
 }
